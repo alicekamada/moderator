c88c9f7e3fa955454d92425ba32fe3e6b9d67872
Cassandra Cruz <celkamada@gmail.com>
2015-03-30 22:32:03 -0700

    Added models for Topics and Questions Added comments for clarity

--- index.js
+++ index.js
@@ -9,11 +9,37 @@ var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
 
+// database intialize
+
 var sequelize = new Sequelize(DBNAME, {
   dialect: 'sqlite',
   storage: './database/data.sqlite'
 });
 
+// models for sequelize
+
+var Topic = sequelize.define('topic', {
+  title: {
+    type: Sequelize.STRING
+  }
+});
+
+var Question = sequelize.define('question', {
+  title: {
+    type: Sequelize.STRING
+  },
+  author: {
+    type: Sequelize.STRING
+  },
+  votesUp: {
+    type: Sequelize.INTEGER
+  },
+  votesDown: {
+    type: Sequelize.INTEGER
+  }
+});
+
+//routes
 
 app.use(express.static(__dirname + '/public'));
 
