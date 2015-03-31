55a3f20d4f506f437264e9189c5b0a22e79876b0
Cassandra Cruz <celkamada@gmail.com>
2015-03-30 19:57:03 -0700

    Added Sequelize dependency to package.json Configured in index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // Configuration options
 
 var PORT = 3000;
+var DBNAME = 'test';
 
 // includes
 
 var express = require('express');
 var app = express();
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DBNAME, {
+  dialect: 'sqlite',
+  storage: './database/data.sqlite'
+});
+
 
 app.use(express.static(__dirname + '/public'));
 
